35187d34fea24f151b87c3f12679c903269160de
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2016-08-21 10:25:16 +0200

    [NO-JIRA]  Win32.

--- src/main/java/be/yildiz/module/sound/openal/OpenAlSoundEngine.java
+++ src/main/java/be/yildiz/module/sound/openal/OpenAlSoundEngine.java
@@ -65,7 +65,7 @@ public final class OpenAlSoundEngine extends SoundEngine implements SoundBuilder
     public OpenAlSoundEngine() {
         super();
         Logger.info("Initializing OpenAL audio engine...");
-        NativeResourceLoader.loadBaseLibrary("libstdc++-6");
+        NativeResourceLoader.loadBaseLibrary("libgcc_s_sjlj-1", "libstdc++-6", "libphysfs", "libsndfile-1", "OpenAL32");
         NativeResourceLoader.loadLibrary("libyildizopenal");
         this.pointer = NativePointer.create(OpenAlSoundEngineNative.initialize());
         Logger.info("OpenAL audio engine initialized.");
