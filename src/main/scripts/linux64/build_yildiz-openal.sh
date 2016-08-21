ff67bcf2748b3cd59502a8f96ef197d47fa95b57
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2016-08-21 09:20:00 +0200

    [NO-JIRA]  Fix CRLF.

--- src/main/scripts/linux64/build_yildiz-openal.sh
+++ src/main/scripts/linux64/build_yildiz-openal.sh
@@ -1,4 +1,4 @@
-cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
+cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
 
 make
 
