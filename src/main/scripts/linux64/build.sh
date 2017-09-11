c62ec5c6caca9d6e89902c19b778d6d38c7412e1
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-11 17:12:08 +0200

    [YIL-324] Make the build fail if native compilation fails.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,8 +1,11 @@
 cmake . -DCMAKE_MODULE_PATH=. -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
 
 make
+r1=$?
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
+
+return $r1
